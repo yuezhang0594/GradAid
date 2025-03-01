d06696a80aa29ef90c8d9882dd303a8f9a5a1a7d
Yue Zhang <yuezhang0594@gmail.com>
2025-03-01 15:11:34 -0500

    init setup

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { LoginForm } from "./components/login-form";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <LoginForm />
+    </>
+  );
+}
+
+export default App;
