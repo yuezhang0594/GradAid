1d3aeb724df4dbe28ced5f83e993bd46cf2a347d
Yue Zhang <yuezhang0594@gmail.com>
2025-03-01 16:14:11 -0500

    Convex auth setup stage 3

--- convex/auth.ts
+++ convex/auth.ts
@@ -1,5 +1,7 @@
+import Google from "@auth/core/providers/google";
+import GitHub from "@auth/core/providers/github";
 import { convexAuth } from "@convex-dev/auth/server";
-
+ 
 export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
-  providers: [],
+  providers: [Google, GitHub],
 });
