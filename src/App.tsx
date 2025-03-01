8f2173fd02a76c286c0d3e506317d8d8deabdde0
Joseph Rissman <jirissman@gmail.com>
2025-03-01 16:01:07 -0500

    convex auth setup stage 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { LoginForm } from "./components/login-form";
 import { cn } from "./lib/utils";
